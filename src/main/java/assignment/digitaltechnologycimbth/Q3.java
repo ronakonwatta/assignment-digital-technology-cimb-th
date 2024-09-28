93fb7b80a96f06566f7157323a21c795fc12c2d4
RONAKON.WATT <kicvong@gmail.com>
2024-09-29 04:26:48 +0700

    note

--- src/main/java/assignment/digitaltechnologycimbth/Q3.java
+++ src/main/java/assignment/digitaltechnologycimbth/Q3.java
@@ -2,6 +2,14 @@ package assignment.digitaltechnologycimbth;
 
 public class Q3 {
     protected int solve(int steps){
+        int oneStep = 1;
+        int twoStep = 2;
+
+        // do not use recursive 
+
+        // use dynamic programing only
+        
+
         return 0;
     }
 }
