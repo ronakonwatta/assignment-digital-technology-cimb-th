a3f4ebaf8baa96eda9239e9984a5256969c525e0
RONAKON.WATT <kicvong@gmail.com>
2024-09-29 03:43:35 +0700

    fix

--- src/main/java/assignment/digitaltechnologycimbth/Q2.java
+++ src/main/java/assignment/digitaltechnologycimbth/Q2.java
@@ -3,36 +3,23 @@ package assignment.digitaltechnologycimbth;
 public class Q2 {
     protected int solve(String version1, String version2) {
 
+        String cleanV1 = removeZeroAfterDot(version1);
+        String cleanV2 = removeZeroAfterDot(version2);
+        double d1 = Double.valueOf(cleanV1);
+        double d2 = Double.valueOf(cleanV2);
+        return Double.compare(d1, d2);
+    }
 
-        // todo ascii check
-
-        char[] ch1 = new char[version1.length()];
-        char[] ch2 = new char[version2.length()];
-
-
-        for (int i = 0; i < version1.length(); i++) {
-            if(("0").equals(version1.charAt(i))) continue;
-
-            ch1[i] = version1.charAt(i);
+    protected String removeZeroAfterDot(String version) {
+        Character zero = Character.valueOf('0');
+        String newVersion = "";
+        for (int i = 0; i < version.length(); i++) {
+            if (version.charAt(0) != zero && zero.compareTo(version.charAt(i)) == 0)
+                continue;
+            newVersion = newVersion + version.charAt(i);
         }
 
-        for (int i = 0; i < version2.length(); i++) {
-            if(("0").equals(version2.charAt(i))) continue;
-
-            ch2[i] = version2.charAt(i);
-        }
-
-        String newCh1 = ch1.toString();
-
-        // int convertVersion1 = Integer.parseInt(newCh1);
-        // int convertVersion2 = Integer.parseInt(ch2.toString());
-
-        // if (convertVersion1 < convertVersion2)
-        //     return -1;
-        // if (convertVersion1 > convertVersion2)
-        //     return 1;
-
-        return 0;
+        return newVersion;
     }
 
 }
